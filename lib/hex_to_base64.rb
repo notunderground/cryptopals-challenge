6defad502d592e397e6be6a02f75c70f57efc919
notunderground <seandelaney@protonmail.com>
2017-08-25 14:47:08 -0700

    finished set1 with specs

--- lib/hex_to_base64.rb
+++ lib/hex_to_base64.rb
@@ -3,8 +3,10 @@ class HexToBase64
   #
   # Cryptopals rule 
   # Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
-  
   require 'base64'
 
-
+  def hex_to_64 s
+    decoded_hex = s.scan(/../).map { |c| c.hex.chr }.join 
+    Base64.strict_encode64 decoded_hex
+  end
 end
