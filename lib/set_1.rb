dfcf38f1d8d05622396daba3a777cdc0606322fd
notunderground <seandelaney@protonmail.com>
2017-08-26 08:53:01 -0700

    create helper function with passing test

--- lib/set_1.rb
+++ lib/set_1.rb
@@ -1,9 +1,9 @@
 class Set1
+  require 'base64'
+  
   # Challenge 1: Convert hex to base64
   # Cryptopals rule 
   # Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
-  require 'base64'
-
   def self.hex_to_64(str)
     decoded_hex = str.scan(/../).map { |c| c.hex.chr }.join 
     Base64.strict_encode64 decoded_hex
@@ -16,4 +16,17 @@ class Set1
     decoded_and_xor.to_s(16)
   end
   
+  # Challenge 3: Single-byte XOR cipher
+  # The hex encoded string: 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+  # ... has been XOR'd against a single character. Find the key, decrypt the message.
+  # You can do this by hand. But don't: write code to do it for you.
+  # How? Devise some method for "scoring" a piece of English plaintext. 
+  # Character frequency is a good metric. Evaluate each output and choose the one with the best score.
+  def self.xor_cypher(str)
+    
+  end
+  
+  def self.hex_to_text(str)
+    str.scan(/../).map { |c| c.hex.chr }.join 
+  end
 end
--- spec/set_1_spec.rb
+++ spec/set_1_spec.rb
@@ -24,4 +24,23 @@ describe Set1 do
       end
     end
   end
+  
+  describe ".xor_cypher" do 
+    context "given hex encoded str find the key and decode message" do
+      it "returns plaintext from encoded str" do
+        encoded_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        
+      end
+    end
+  end
+  
+  describe ".hex_to_text" do
+    context "given string of 8 byte hex convert to chars" do
+      it "returns plaintext string" do
+        str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        expect(Set1.hex_to_text(str)).to eql("I'm killing your brain like a poisonous mushroom")
+      end
+      
+    end
+  end
 end
